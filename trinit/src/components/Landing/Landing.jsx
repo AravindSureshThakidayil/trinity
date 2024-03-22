9e1a08856cc0b3fda3cfe5678f6da9cc321df08f
tzzkenz <127670903+tzzkenz@users.noreply.github.com>
2024-03-22 22:38:03 +0530

    added an image for the body page

--- trinit/src/components/Landing/Landing.jsx
+++ trinit/src/components/Landing/Landing.jsx
@@ -1,9 +1,23 @@
 import Header from "../Header/Header";
+import imagesrc from "./landing.png";
 
 function Landing() {
   return (
     <>
       <Header />
+      <main>
+        <div className="some-thing">
+          <div className="Heading">Microloans at your fingertips</div>
+          <div className="body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            vestibul ante vitae risus laoreet, id interdum sapien fermentum.
+            Nulla facilisi. Phasellus lobortis vehicula arcu, id viverra ligula
+            tempor vel. Quisque malesuada, velit a fringilla gravida, mauris leo
+            fermentum dolor, nec.
+          </div>
+          <img src={imagesrc} alt="image" />
+        </div>
+      </main>
     </>
   );
 }
