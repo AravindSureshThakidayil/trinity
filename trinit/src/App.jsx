18ac5430d29edd84a1e0ca03dfe23f74fed12d83
tzzkenz <127670903+tzzkenz@users.noreply.github.com>
2024-03-23 11:31:44 +0530

    Revert "A lot more pages added"
    This reverts commit 0cc93fa7f278dab63c2898beb29f2cb0f92e447e.

--- trinit/src/App.jsx
+++ trinit/src/App.jsx
@@ -7,40 +7,21 @@ import Lend from "./components/Lend/Lend";
 import LendDetails from "./components/LendDetails/LendDetails";
 import Borrow from "./components/Borrow/Borrow";
 import BorrowDetais from "./components/BorrowDetails/BorrowDetails";
-import Pay1 from "./components/Pay1/Pay1";
-import Pay2 from "./components/Pay2/Pay2";
-import { ThirdwebProvider, useContract } from "@thirdweb-dev/react";
-import Success from "./components/Success/Success";
 
 export default function App() {
-  return (
-    <>
-      <ThirdwebProvider
-        activeChain="sepolia"
-        clientId="1e0827a6926e39bddcba11b1fdc0439fD" // You can get a client id from dashboard settings
-      >
-        <Component />
-      </ThirdwebProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="signIn" element={<SignIn />} />
-          <Route path="signUp" element={<SignUp />} />
-          <Route path="lend" element={<Lend />} />
-          <Route path="lendDetails" element={<LendDetails />} />
-          <Route path="borrow" element={<Borrow />} />
-          <Route path="borrowDetails" element={<BorrowDetais />} />
-          <Route path="pay1" element={<Pay1 />} />
-          <Route path="pay2" element={<Pay2 />} />
-          <Route path="success" element={<Success />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
-}
+  
 
-function Component() {
-  const { contract, isLoading } = useContract(
-    "0x73e59F2D84dD4aBb9a88FE829f91b889453B2763"
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="signIn" element={<SignIn />} />
+        <Route path="signUp" element={<SignUp />} />
+        <Route path="lend" element={<Lend />} />
+        <Route path="lendDetails" element={<LendDetails />} />
+        <Route path="borrow" element={<Borrow />} />
+        <Route path="borrowDetails" element={<BorrowDetais />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
